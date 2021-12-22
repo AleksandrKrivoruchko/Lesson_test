1e753a7f7f61a050e0f89fd4fd59b094ce666596
AleksandrKrivoruchko <avk741120@yandex.ru>
2021-12-22 21:07:22 +0300

    My first project is started work.

---
+++ example001/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число ");
+string test = Console.ReadLine();
+int number = int.Parse(test);
+Console.WriteLine("Вы ввели число " + number);
