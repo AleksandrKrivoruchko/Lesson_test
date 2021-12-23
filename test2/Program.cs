607348e1ebd7aaba8bb11c03d3d3235e77c7a524
AleksandrKrivoruchko <avk741120@yandex.ru>
2021-12-23 16:50:44 +0300

    Добавлено решение третьего задания

---
+++ test2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите первое число ");
+try
+{
+    double numberA = double.Parse(Console.ReadLine());
+    Console.Write("Введите второе число ");
+    double numberB = double.Parse(Console.ReadLine());
+    if (numberA > numberB)
+    {
+        Console.WriteLine("Чило {0} больше числа {1}", numberA, numberB);
+    }
+    else if (numberB > numberA)
+    {
+        Console.WriteLine("Чило {0} больше числа {1}", numberB, numberA);
+    }
+    else
+    {
+        Console.WriteLine("Чило {0} равно числу {1}", numberB, numberA);
+    }
+}
+catch (FormatException)
+{
+    Console.WriteLine("Неправильный ввод");
+}
