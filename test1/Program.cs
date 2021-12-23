ccba9afc714fb9338c311c992c84d64ff56727a7
AleksandrKrivoruchko <avk741120@yandex.ru>
2021-12-23 13:46:32 +0300

    Добавлено решение задания 1 (проверка является ли одно из двух чисел квадратом другого числа)

---
+++ test1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите первое число ");
+try
+{
+    double numberA = double.Parse(Console.ReadLine());
+    Console.Write("Введите второе число ");
+    double numberB = double.Parse(Console.ReadLine());
+    if (numberA * numberA == numberB)
+    {
+        Console.WriteLine("Число {0} является квадратом числа {1}", numberB, numberA);
+    }
+    else if (numberB * numberB == numberA)
+    {
+        Console.WriteLine("Число {0} является квадратом числа {1}", numberA, numberB);
+    }
+    else
+    {
+        Console.Write("Первое число не является квадратом второго, ");
+        Console.WriteLine("а второе не является квадратом первого");
+    }
+}
+catch (FormatException)
+{
+    Console.WriteLine("Неправильный ввод");
+}
