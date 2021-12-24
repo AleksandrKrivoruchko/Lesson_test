0be6a999047e5258332ae7858d9027e2cda84d77
AleksandrKrivoruchko <avk741120@yandex.ru>
2021-12-24 15:58:28 +0300

    Добавлено решение седьмой задачи (определить является ли число чётным)

---
+++ test6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число ");
+try
+{
+    int value = int.Parse(Console.ReadLine());
+    if (value % 2 == 0)
+        Console.WriteLine("Число чётное");
+    else
+        Console.WriteLine("Число не чётное");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Введено не целое число ");
+}
