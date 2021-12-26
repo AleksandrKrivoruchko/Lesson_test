f4f5a95529e35766761eb2d50aab8f7d542d3cf0
AleksandrKrivoruchko <avk741120@yandex.ru>
2021-12-26 20:06:32 +0300

    Добавлено решение задания 15 (дано число, проверить кратно ли оно 7 и 23)

---
+++ test15/Program.cs
@@ -0,0 +1,27 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+int my_read_int(string msg) //возвращает целое число с консоли
+{
+    Console.Write(msg);
+    string answer = Console.ReadLine() ?? String.Empty;
+    if (int.TryParse(answer, out int value))
+        return value;
+    else
+        return -1;
+}
+
+Console.Clear();
+//Отладочный цикл
+while (Console.ReadLine() != "q")
+{
+    int value1 = my_read_int("Введите целое число: ");
+    if (value1 == -1)
+    {
+        Console.WriteLine("Не корректный ввод данных");
+        continue;
+    }
+    if (value1 % 7 == 0 && value1 % 23 == 0)
+        Console.WriteLine($"Число {value1} кратно 7 и 23");
+    else
+        Console.WriteLine($"Число {value1} не крато 7 и 23");
+    Console.WriteLine("Для выхода из программы введите q");
+}
