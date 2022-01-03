7645fed42ad732b168ecd5bf26b6e94c743b5ff1
AleksandrKrivoruchko <avk741120@yandex.ru>
2022-01-03 21:39:07 +0300

    Добавлено решение задания 26 (Возведите число А в натуральную степень В используя цикл)

---
+++ test26/Program.cs
@@ -0,0 +1,39 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+int my_input(string msg, int marker=0)
+{
+    Console.Write(msg);
+    string temp_str = Console.ReadLine() ?? String.Empty;
+    bool flag = int.TryParse(temp_str, out int value);
+    if(!flag || (marker > 0 && value < 0))
+    {
+        Console.WriteLine("Неверный ввод");
+        return -1;
+    }
+    return value;
+}
+
+bool answer()
+{
+    Console.WriteLine("Введите q для выхода из программы, для продолжения нажмите Enter.");
+    string flag = Console.ReadLine() ?? String.Empty;
+    if(flag == "q")
+        return false;
+    return true;
+}
+
+while(answer())
+{
+    int value = my_input("Введите число: ");
+    if(value == -1)
+        continue;
+    int power = my_input("Введите степень: ", 1);
+    if(power == -1)
+        continue;
+    int result = 1;
+    for (int i = 0; i < power; i++)
+    {
+        result *= value;
+    }
+    Console.WriteLine(result);
+}
