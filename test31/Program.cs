64d9b5bb97c3056d80ef44500adff0ef1b10585d
AleksandrKrivoruchko <avk741120@yandex.ru>
2022-01-10 11:52:31 +0300

    Добавлено решение заданий 31 и 32

---
+++ test31/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задать массив из 8 элементов и вывести их на экран 
+
+void FillRandArray(int[] array, int start, int end)
+{
+    Random randValue = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = randValue.Next(start, end);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}\t");
+        if (i % 7 == 0 && i != 0)
+            Console.WriteLine();
+    }
+}
+
+bool IsExit()
+{
+    Console.WriteLine("Введите q для завершения, для продолжения нажмите Enter");
+    string index = Console.ReadLine() ?? string.Empty;
+    if (index == "q")
+        return false;
+    return true;
+}
+
+int[] array = new int[20];
+while (IsExit())
+{
+    FillRandArray(array, -10, 10);
+    PrintArray(array);
+    Console.WriteLine();
+}
---
+++ test32/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+void FillRandArray(int[] array, int start, int end)
+{
+    Random randValue = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = randValue.Next(start, end + 1);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}\t");
+        if (i % 7 == 0 && i != 0)
+            Console.WriteLine();
+    }
+}
+
+bool IsExit()
+{
+    Console.WriteLine("Введите q для завершения, для продолжения нажмите Enter");
+    string index = Console.ReadLine() ?? string.Empty;
+    if (index == "q")
+        return false;
+    return true;
+}
+
+int[] array = new int[20];
+while (IsExit())
+{
+    FillRandArray(array, 0, 1);
+    PrintArray(array);
+    Console.WriteLine();
+}
