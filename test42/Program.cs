d2f8318fe2f2a0742f899de1bfe335d200495b0e
AleksandrKrivoruchko <avk741120@yandex.ru>
2022-01-13 22:11:45 +0300

    Добавлено решение задания 42

---
+++ test42/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+// Определить сколько чисел больше 0 введено с клавиатуры
+string MyInput(string msg)
+{
+    WriteLine(msg);
+    return ReadLine() ?? string.Empty;
+}
+
+int positive = 0;
+string[] str = { "чисел", "число", "числа" };
+string answer = string.Empty;
+while (answer != "q")
+{
+    answer = MyInput("Введите число (для завершения введите q");
+    if (double.TryParse(answer, out double value))
+        if (value > 0)
+            positive++;
+}
+if (positive % 10 == 1)
+    answer = str[1];
+else if (positive % 10 > 1 && positive % 10 < 5)
+    answer = str[2];
+else
+    answer = str[0];
+
+WriteLine($"С клавиатуры введено {positive} {answer} больше нуля");
