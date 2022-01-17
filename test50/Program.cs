08eb896cfd6eb4ca02b30f845e90312fcc03990a
AleksandrKrivoruchko <avk741120@yandex.ru>
2022-01-17 12:52:51 +0300

    Добавлено решение задания 50

---
+++ test50/Program.cs
@@ -0,0 +1,36 @@
+﻿// В двумерном массиве n×k заменить четные элементы на противоположные
+using static System.Console;
+
+int[,] CreateTwoDimensionArray(int row, int column, int min, int max)
+{
+    int[,] result = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < result.GetLength(0); i++)
+        for (int j = 0; j < result.GetLength(1); j++)
+            result[i, j] = rnd.Next(min, max);
+    return result;
+}
+
+void PrintTwoDimensionArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Write($"{array[i, j]}\t");
+        WriteLine();
+    }
+    WriteLine();
+}
+
+int[,] ChangeEvenElement(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            if (array[i, j] % 2 == 0) array[i, j] *= -1;
+    return array;
+}
+
+int[,] arrayTwoDimension = CreateTwoDimensionArray(4, 5, 0, 100);
+PrintTwoDimensionArray(arrayTwoDimension);
+WriteLine();
+PrintTwoDimensionArray(ChangeEvenElement(arrayTwoDimension));
