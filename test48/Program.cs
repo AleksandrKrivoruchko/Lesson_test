8b02abec398961f928f67ee55335157d01aef7f8
AleksandrKrivoruchko <avk741120@yandex.ru>
2022-01-17 11:10:18 +0300

    Добавлено решение задания 48

---
+++ test48/Program.cs
@@ -0,0 +1,25 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами
+using static System.Console;
+
+int[,] CreateTwoDimensionArray(int row, int column, int min, int max)
+{
+    int[,] result = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < result.GetLength(0); i++)
+        for (int j = 0; j < result.GetLength(1); j++)
+            result[i, j] = rnd.Next(min, max);
+    return result;
+}
+
+void PrintTwoDimensionArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Write($"{array[i, j]}\t");
+        WriteLine();
+    }
+}
+
+int[,] arrayTwoDimension = CreateTwoDimensionArray(10, 8, 0, 100);
+PrintTwoDimensionArray(arrayTwoDimension);
