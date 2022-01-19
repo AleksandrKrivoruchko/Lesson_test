e4e86ee6140f66a799a9a5356c8d0a49d7f481a7
AleksandrKrivoruchko <avk741120@yandex.ru>
2022-01-19 06:40:43 +0300

    Набросок решения задания 60

---
+++ test60/Program.cs
@@ -0,0 +1,36 @@
+﻿// Составить частотный словарь элементов двумерного массива
+using static System.Console;
+
+int[,] CreateTwoDimensionArray(int row, int column, int min, int max)
+{
+    int[,] result = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < result.GetLength(0); i++)
+        for (int j = 0; j < result.GetLength(1); j++)
+            result[i, j] = rnd.Next(min, max);
+    return result;
+}
+
+void PrintTwoDimensionArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Write($"{array[i, j]}\t");
+        WriteLine();
+    }
+    WriteLine();
+}
+
+bool IsFindElement(int[,] array, int[] elementArray)
+{
+    for (int i = ++elementArray[1]; i < array.GetLength(0); i++)
+        for (int j = ++elementArray[2]; j < array.GetLength(1); j++)
+            if (array[i, j] == elementArray[0])
+            {
+                elementArray[1] = i;
+                elementArray[2] = j;
+                return true;
+            }
+    return false;
+}
