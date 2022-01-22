d5347de3ec5b5f20bf5b0c86bba9657156560d7e
AleksandrKrivoruchko <avk741120@yandex.ru>
2022-01-22 21:51:08 +0300

    Добавлено решение задания 66

---
+++ test66/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать натуральные числа от 1 до N, N задано
+int NumericalSeries(int value, int range)
+{
+    Console.Write($"{value}\t");
+    if (value % 10 == 0)
+        Console.WriteLine();
+    while (value != range)
+        return NumericalSeries(value + 1, range);
+    return value;
+}
+
+NumericalSeries(1, 87215);
+Console.WriteLine();
