54b81f5e0a4d4867f0fa5938a1820703c3f0e5ea
AleksandrKrivoruchko <avk741120@yandex.ru>
2022-01-24 10:08:59 +0300

    Добавлено решение задания 67

---
+++ test67/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать натуральные числа от N до 1, N задано
+using static System.Console;
+int NumericalSeries(int range)
+{
+    Write($"{range}\t");
+    if (range % 11 == 0)
+        WriteLine();
+    if (range == 1)
+        return 1;
+    return NumericalSeries(range - 1);
+}
+
+NumericalSeries(1000);
+WriteLine();
