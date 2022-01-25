4e97e4421455138ecdeac703545cc00e9724b2a9
AleksandrKrivoruchko <avk741120@yandex.ru>
2022-01-25 16:52:16 +0300

    Добавлен макет решения задания 72

---
+++ test72/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу возведения числа А в целую стень B
+double RecursionPowerInt(double number, int power)
+{
+    if (power == 0) return 1;
+    return number * RecursionPowerInt(number, power - 1);
+}
+
+Console.Write("Введите число: ");
+double number = 0;
+string inputNumber = Console.ReadLine() ?? string.Empty;
+bool isNumber = double.TryParse(inputNumber, out number);
+Console.Write("Введите степень: ");
+int power = 0;
+inputNumber = Console.ReadLine() ?? string.Empty;
+isNumber = int.TryParse(inputNumber, out power);
+
+Console.WriteLine(RecursionPowerInt(number, power));
