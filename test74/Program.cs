665f91d5b4c38e806bf7b1cef8282cdbd91a8718
AleksandrKrivoruchko <avk741120@yandex.ru>
2022-01-26 22:40:16 +0300

    Макет решения задания 74

---
+++ test74/Program.cs
@@ -0,0 +1,31 @@
+﻿// В некотором машинном алфавите имеются четыре буквы «а», «и», «с» и «в».
+// Покажите все слова, состоящие из n букв,
+// которые можно построить из букв этого алфавита
+int PowerInt(int number, int power)
+{
+    int degree = 1;
+    for (int i = 0; i < power; i++)
+    {
+        degree *= number;
+    }
+    return degree;
+}
+
+char[] charArray = { 'а', 'и', 'с', 'в' };
+int countChars = 2;
+int count = 0;
+int iteration = PowerInt(countChars, charArray.Length);
+string[] resultArray = new string[iteration];
+
+for (int i = 0; i < charArray.Length; i++)
+{
+    for (int j = 0; j < charArray.Length; j++)
+    {
+        resultArray[count] = $"{charArray[i]}{charArray[j]}";
+        count++;
+    }
+}
+for (int i = 0; i < resultArray.Length; i++)
+{
+    Console.WriteLine($"{i}\t{resultArray[i]}");
+}
