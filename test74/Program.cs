2ab322ebdf1436fb64755944c19e34d0626b1e8e
AleksandrKrivoruchko <avk741120@yandex.ru>
2022-01-27 17:12:00 +0300

    Добавлен макет функции создания слов

--- test74/Program.cs
+++ test74/Program.cs
@@ -11,6 +11,42 @@ int PowerInt(int number, int power)
     return degree;
 }
 
+string RecursivMakeWord(char[] charArray, int countChar)
+{
+    string result = string.Empty;
+    if (countChar == 0) return string.Empty;
+    for (int i = 0; i < charArray.Length; i++)
+    {
+        result = result + $"{charArray[i]}" + RecursivMakeWord(charArray, countChar - 1);
+    }
+    return result;
+}
+
+// string[] MakeWord(char[] charArray, int indexChar, int countChar)
+// {
+//     string[] resultWords = new string[charArray.Length];
+//     int count = 0;
+//     int indexWord = 0;
+//     resultWords[indexWord] += $"{charArray[indexChar]}";
+//     for (int i = 0; i < charArray.Length; i++)
+//     {
+//         if (count < countChar - 1)
+//         {
+//             resultWords[indexWord] = resultWords[indexWord] + $"{charArray[i]}";
+//             count++;
+//         }
+//         else
+//         {
+//             Console.WriteLine(resultWords[indexWord]);
+//             indexWord++;
+//             count = 0;
+//             resultWords[indexWord] = resultWords[indexWord] + $"{charArray[indexChar]}";
+//             i--;
+//         }
+//     }
+//     return resultWords;
+// }
+
 char[] charArray = { 'а', 'и', 'с', 'в' };
 int countChars = 2;
 int count = 0;
@@ -29,3 +65,13 @@ for (int i = 0; i < resultArray.Length; i++)
 {
     Console.WriteLine($"{i}\t{resultArray[i]}");
 }
+
+string str = RecursivMakeWord(charArray, 2);
+Console.WriteLine(str);
+
+// string[] universalArray = MakeWord(charArray, 0, 3);
+
+// for (int i = 0; i < universalArray.Length; i++)
+// {
+//     Console.WriteLine($"{i}\t{universalArray[i]}");
+// }
