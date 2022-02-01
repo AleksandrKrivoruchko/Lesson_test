e0d2a3e025e355fa4f498ff85bea339988596dca
AleksandrKrivoruchko <avk741120@yandex.ru>
2022-02-01 12:38:34 +0300

    Добавлено решение задания 75

--- test75/Program.cs
+++ test75/Program.cs
@@ -8,7 +8,7 @@ using static System.Console;
 int Power(int number, int degree)
 {
     int result = 1;
-    if (degree == 0) return 1;
+    if (degree == 0) return result;
     for (int i = 0; i < degree; i++)
     {
         result *= number;
@@ -39,6 +39,11 @@ void PrintArray(int[] array)
 
 int[] ConvertBinaryArrayToDecimal(int[] arrayData, int[] arrayInfo)
 {
+    bool flag = IsCanConvert(arrayData, arrayInfo);
+    if (!flag)
+    {
+        Environment.Exit(1);
+    }
     int[] resultArray = new int[arrayInfo.Length];
     int indexStart = 0;
     int indexEnd = 0;
@@ -51,10 +56,35 @@ int[] ConvertBinaryArrayToDecimal(int[] arrayData, int[] arrayInfo)
     return resultArray;
 }
 
+int SumElementsArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+
+bool IsCanConvert(int[] arrayData, int[] arrayInfo)
+{
+    int answer = arrayData.Length - SumElementsArray(arrayInfo);
+    if (answer == 0) return true;
+    if (answer > 0)
+    {
+        WriteLine("В массиве info не достаточно информации.");
+        return false;
+    }
+    else
+    {
+        WriteLine("В массиве data не достаточно информации.");
+        return false;
+    }
+}
 
 int[] data = { 1, 1, 1, 0, 1, 0, 1, 0, 1 };
 int[] info = { 2, 3, 3, 1 };
-int[] resultArray = ConvertBinaryArrayToDecimal(data, info);
 PrintArray(data);
 PrintArray(info);
+int[] resultArray = ConvertBinaryArrayToDecimal(data, info);
 PrintArray(resultArray);
