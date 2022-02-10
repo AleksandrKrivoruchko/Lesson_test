6fbf72ff96e1016ec7e4e935073c92ebea7c8781
AleksandrKrivoruchko <avk741120@yandex.ru>
2022-02-10 10:03:41 +0300

    Finalization of task 3

--- test3/Program.cs
+++ test3/Program.cs
@@ -1,15 +1,10 @@
-﻿string[] week_days = { "Понедельник", "Вторник", "Среда", "Четверг",
+﻿using static System.Console;
+using static NewMethod.MyMethod;
+
+string[] weekDays = { "Понедельник", "Вторник", "Среда", "Четверг",
                         "Пятница", "Суббота", "Воскресенье"};
-Console.Write("Введите номер дня недели ");
-try
-{
-    int week_day = int.Parse(Console.ReadLine());
-    if (week_day < 8 && week_day > 0)
-        Console.WriteLine("Вы ввели {0}й день это {1}", week_day, week_days[week_day - 1]);
-    else
-        Console.WriteLine("Нет такого дня недели {0}", week_day);
-}
-catch (FormatException)
-{
-    Console.WriteLine("Введено не целое число");
-}
+int weekDay = InputInt("Введите номер дня недели");
+if (weekDay < 8 && weekDay > 0)
+    Console.WriteLine("Вы ввели {0}й день это {1}", weekDay, weekDays[weekDay - 1]);
+else
+    Console.WriteLine("Нет такого дня недели {0}", weekDay);
